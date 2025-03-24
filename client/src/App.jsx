115775fdeaee8342df924ff1dec826e92f877e95
Israel Akinoso <akinosoissy@gmail.com>
2025-03-24 18:52:05 +0000

    Typography setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import { Button } from "./components/ui/button";
 
 function App() {
-  return <>App
-  
-  <Button>Add</Button>  </>;
+  return (
+    <>
+      Hello Apeture say hi 0123456789
+      <h1 className="font- text-8xl font-bold">Title a sans</h1>
+      <Button>Add</Button>{" "}
+    </>
+  );
 }
 
 export default App;
