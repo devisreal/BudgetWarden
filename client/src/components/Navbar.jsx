2fa1ae65ed3b845fbf246e593705d808f63fd230
Israel Akinoso <akinosoissy@gmail.com>
2025-03-25 17:36:43 +0000

    Work in progress...

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,22 +2,19 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className=" bg-emerald-50 py-4 px-2 sticky top-0 z-[100]">
+    <nav className="bg-white/70 backdrop-blur-sm py-4 px-2 sticky top-0 z-[100]">
       <div className="max-w-[85rem] mx-auto flex justify-between items-center">
-        <h1 className="font-itle text-2xl font-bold tracking-tighter text-neutral-900">
+        <Link
+          to="/"
+          className="font-itle text-2xl font-bold tracking-tighter text-neutral-900"
+        >
           Budget Warden
-        </h1>
+        </Link>
 
         <div className="flex">
           <ul className="flex items-center gap-4">
-            <li className="inline-flex items-center rounded-full transition-colors duration-200 hover:bg-emerald-200 px-3.5 py-1 text-md font-semibold text-neutral-900">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="inline-flex items-center rounded-full transition-colors duration-200 hover:bg-emerald-200 px-3.5 py-1 text-md font-semibold text-neutral-900">
-              <Link to="/about">About</Link>
-            </li>
-            <li className="inline-flex items-center rounded-full transition-colors duration-200 hover:bg-emerald-200 px-3.5 py-1 text-md font-semibold text-neutral-900">
-              <Link to="">Get Started</Link>
+            <li className="inline-flex items-center rounded-full transition-colors duration-200 bg-emerald-700 px-5 py-2.5 text-md font-medium text-white">
+              <Link to="/auth/register">Get Started</Link>
             </li>
           </ul>
         </div>
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,42 +1,38 @@
+import { Ripple } from "@/components/magicui/ripple";
 import React from "react";
 
 export default function HomePage() {
   return (
     <div className="relative">
-      <section className="bg-emerald-50">
-        <div className="p-8 md:p-12 lg:px-16 lg:h-[60vh] lg:grid lg:place-content-center">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-emerald-600">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
-          <div className="mx-auto max-w-[80rem] text-center">
-            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-7xl font-title">
-              Take Full Control Of <br /> Your Personal Finances
-            </h2>
+      <section className="relative flex h-[40rem] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
+        <div className="mx-auto max-w-[80rem] text-center">
+          <h2 className="text-3xl font-extrabold tracking-tight leading-[1] text-gray-950 md:text-[5rem] font-title">
+            Take Full Control Of <br /> Your Personal Finances
+          </h2>
 
-            <p className="hidden text-gray-500 sm:mt-4 sm:block">
-              Manage and Visualize your everyday finances with features you’ll
-              love to use.
-            </p>
-          </div>
-
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-            >
-              Get started
-            </a>
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-              Learn more <span aria-hidden="true">→</span>
-            </a>
-          </div>
+          <p className="text-gray-500 sm:mt-4 font-medium text-lg">
+            Manage and Visualize your everyday finances with features you’ll
+            love to use.
+          </p>
         </div>
+
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="#"
+            className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+          >
+            Get started
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Learn more <span aria-hidden="true">→</span>
+          </a>
+        </div>
+
+        <Ripple
+          mainCircleOpacity={0.17}
+          mainCircleSize={300}
+          numCircles={100}
+        />
       </section>
 
       <br />
