232aff209ebd5d480d2ff1160fcd2cc18c3d751b
Israel Akinoso <akinosoissy@gmail.com>
2025-04-07 16:38:26 +0100

    Attempted fix for vite url using fileURLToPath

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    // alias: {
+    //   "@": path.resolve(__dirname, "./src"),
+    // },
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
   },
 });
