f7cb1c7cb1d9caebfdccb7b281ecdb15d95d1dcb
Israel Akinoso <akinosoissy@gmail.com>
2025-04-07 16:19:33 +0100

    Trying another fix to vercel resolve url paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@/": path.join(__dirname, "src/"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
